1fdb0d54770e6c4f1c4f963ef32138c70f8ab1eb
Dark Eyes <dark_eyes_@hotmail.com>
2016-01-21 22:46:21 -0500

    Add options to hide next alarm in status bar and lockscreen (2/3)
    Change-Id: I03bd8430480918c9da309f1de1a2d146000df72c

--- src/com/android/deskclock/SettingsActivity.java
+++ src/com/android/deskclock/SettingsActivity.java
@@ -60,6 +60,7 @@ public class SettingsActivity extends BaseActivity {
     public static final String KEY_VOLUME_BUTTONS = "volume_button_setting";
     public static final String KEY_SHOW_ALARM_ICON = "show_status_bar_icon";
     public static final String KEY_ALARM_SETTINGS = "key_alarm_settings";
+    public static final String KEY_SHOW_NEXT_ALARM = "show_next_alarm";
 
     public static final String DEFAULT_VOLUME_BEHAVIOR = "0";
     public static final String VOLUME_BEHAVIOR_SNOOZE = "1";
@@ -122,6 +123,11 @@ public class SettingsActivity extends BaseActivity {
                     (SwitchPreference) findPreference(KEY_SHOW_ALARM_ICON);
             showAlarmIconPref.setChecked(CMSettings.System.getInt(
                     getActivity().getContentResolver(), CMSettings.System.SHOW_ALARM_ICON, 1) == 1);
+
+            final SwitchPreference showNextAlarmPref =
+                    (SwitchPreference) findPreference(KEY_SHOW_NEXT_ALARM);
+            showNextAlarmPref.setChecked(CMSettings.System.getInt(
+                    getActivity().getContentResolver(), CMSettings.System.SHOW_NEXT_ALARM, 1) == 1);
         }
 
         @Override
@@ -164,6 +170,9 @@ public class SettingsActivity extends BaseActivity {
             } else if (KEY_SHOW_ALARM_ICON.equals(pref.getKey())) {
                 CMSettings.System.putInt(getActivity().getContentResolver(),
                         CMSettings.System.SHOW_ALARM_ICON, (Boolean) newValue ? 1 : 0);
+            } else if (KEY_SHOW_NEXT_ALARM.equals(pref.getKey())) {
+                CMSettings.System.putInt(getActivity().getContentResolver(),
+                        CMSettings.System.SHOW_NEXT_ALARM, (Boolean) newValue ? 1 : 0);
             }
             // Set result so DeskClock knows to refresh itself
             getActivity().setResult(RESULT_OK);
@@ -287,6 +296,10 @@ public class SettingsActivity extends BaseActivity {
             final SwitchPreference showAlarmIconPref =
                     (SwitchPreference) findPreference(KEY_SHOW_ALARM_ICON);
             showAlarmIconPref.setOnPreferenceChangeListener(this);
+
+            final SwitchPreference showNextAlarmPref =
+                    (SwitchPreference) findPreference(KEY_SHOW_NEXT_ALARM);
+            showNextAlarmPref.setOnPreferenceChangeListener(this);
         }
 
         private void updateAutoSnoozeSummary(ListPreference listPref, String delay) {
